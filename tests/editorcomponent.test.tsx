bb652e5edf32c2268b8d63833f3a9180e97fd26a
yohance-lorrington <tabahanih@gmail.com>
2019-06-07 21:54:44 -0400

    added setup for react component testing

---
+++ tests/editorcomponent.test.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as enzyme from 'enzyme';
+import Editor from '../src/componenets/NodeEditor/Editor';
+
+describe('<Editor/>',()=>{
+    test('it does a render :)',()=>{
+        const wrapper = enzyme.shallow(<Editor/>);
+        
+        expect(wrapper.length).toBe(1);
+    })
+})
